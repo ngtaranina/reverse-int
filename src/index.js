5abde86a7f282e8c9f1a40b038f57559325a6875
Nataliya Taranina <ngtaranina@hotmail.com>
2022-07-20 20:16:32 +0300

    Solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    number = Math.abs(number);
+    const str_Number = String(number);
+    const arr_Number = [];
+    const length_Number = str_Number.length;
+
+    
+/* Переводим заданное число в массив */
+    for (let i=0; i < length_Number; i++) {
+        arr_Number[i] = str_Number[i];
+      }
+      
+/* Меняем элементы */
+    let arr_Number_help = [];
+    for ( let i=0; i < length_Number; i++){
+        arr_Number_help[i] = arr_Number[length_Number-1-i];
+        
+    }
+
+ /* Убираем нули впереди */   
+
+    for ( let i=0; i < ( length_Number - 1 ); i++){
+        if ( arr_Number_help[0] == 0 ){
+            arr_Number_help.shift();
+        }
+     }
+    
+/* Выводим массив в строку */
+    let rezult = ""
+    for (let unit of arr_Number_help) {
+        rezult = rezult + unit;
+    }
+    console.log (rezult);
+    return (rezult);
 }
